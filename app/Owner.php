a1baabb05ed715feb1b72673eafaa20ab68a0aaa
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-17 16:34:10 +0100

    define table relationships

--- app/Owner.php
+++ app/Owner.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //
+    protected $table = "owners";
+
+    protected $fillable = [
+        "name",
+        "copyright"
+
+    ];
+
+    public function photos() {
+        return $this->hasMany( Photo::class );
+    }
 }
--- app/Photo.php
+++ app/Photo.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+    protected $table = "photos";
+
+    protected $fillable = [
+        
+        "title",
+        "caption",
+        "copyright",
+        "email",
+        "owner_id"
+
+    ];
+
+    public function owner() {
+        return $this->belongsTo( Owner::class );
+
+    }
 }
