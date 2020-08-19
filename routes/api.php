eea6c6178cb59df1d375e2a7168783b05783e500
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-19 12:21:15 +0100

    comment default route

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
 
 
 Route::get('photos','PhotoController@index');
-
 Route::get('owners','OwnerController@index');
