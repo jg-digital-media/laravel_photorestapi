c1e67f141329b118dd3dda23e025576976d23a7d
jg-digital-media <mail@jonniegrieve.co.uk>
2020-09-16 14:16:04 +0100

    comment for original return

--- app/Http/Resources/OwnerResource.php
+++ app/Http/Resources/OwnerResource.php
@@ -14,6 +14,9 @@ class OwnerResource extends JsonResource
      */
   
     public function toArray($request) {
+
+        //return parent::toArray($request);
+        
         return [
             'name' => $this->name,
             'copyright' => $this->copyright
